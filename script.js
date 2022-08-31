1d89b3a60a2fe0d109751fadf0accb87b0fd01b9
Timothy <timothy.2k21@gmail.com>
2022-08-31 20:15:10 +0300

    added api

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fc5e557b35msh8125f43616ed73ap100d7ejsn2eba6b607a8f',
+		'X-RapidAPI-Host': 'billboard-api2.p.rapidapi.com'
+	}
+};
+
+fetch('https://billboard-api2.p.rapidapi.com/hot-100?range=1-10&date=2019-05-11', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+
 const fom = document.querySelector("#form1");
 fom.addEventListener("submit",forme)
 
