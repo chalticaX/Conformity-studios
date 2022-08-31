5b6c2d8d228985f5b94a5319b0e546e09ad5f66c
Timothy <timothy.2k21@gmail.com>
2022-08-31 18:24:37 +0300

    deletion of overiding code

--- script.js
+++ script.js
@@ -1,58 +1,12 @@
-$(document).ready(function(){
-    $("#designIcon").click(function(){
-        $("#design").show()
-    })
-    $("#design").click(function(){
-        $("#design").hide()
-    })
-    $("#devIcon").click(function(){
-        $("#development").show()
-    })
-    $("#development").click(function(){
-        $("#development").hide()
-    })
-    $("#productIcon").click(function(){
-        $("#management").show()
-    })
-    $("#management").click(function(){
-        $("#management").hide()
-    })
-    $(".one").mouseover(function(){
-        $("#hover1").toggle()
-    })
-    $(".two").mouseover(function(){
-        $("#hover2").toggle()
-    })
-    $(".three").mouseover(function(){
-        $("#hover3").toggle()
-    })
-    $(".four").mouseover(function(){
-        $("#hover4").toggle()
-    })
-    $(".five").mouseover(function(){
-        $("#hover5").toggle()
-    })
-    $(".six").mouseover(function(){
-        $("#hover6").toggle()
-    })
-    $(".seven").mouseover(function(){
-        $("#hover7").toggle()
-    })
-    $(".eight").mouseover(function(){
-        $("#hover8").toggle()
-    })
-})
-
-
-var fom = document.querySelector("#form1");
+const fom = document.querySelector("#form1");
 fom.addEventListener("submit",forme)
 
 function forme(event){
     event.preventDefault()
 
-    var userName = document.querySelector("#names").value;
-    var userEmail = document.querySelector("#emails").value;
-    var userMessages = document.querySelector("#messages").value;
+    const userName = document.querySelector("#names").value;
+    const userEmail = document.querySelector("#emails").value;
+    const userMessages = document.querySelector("#messages").value;
 
     if(userName == ""){
         return alert("please fill in name")
